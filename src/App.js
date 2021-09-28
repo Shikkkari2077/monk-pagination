4c8fce29acd82e328d45715529d47808be6fdfbb
Kapil <kapilprajapati061@gmail.com>
2021-09-28 16:29:52 +0530

    clear

--- src/App.js
+++ src/App.js
@@ -50,6 +50,7 @@ function App() {
                     <option value="10">10</option>
                     <option value="20">20</option>
                     <option value="50">50</option>
+                    <option value="100">100</option>
         </select>
         </li>
       </ul>
